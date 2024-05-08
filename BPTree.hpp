c2a34df25f13a604c4885dcee58a33ca75347f5a
skywong14 <skywong0014@gmail.com>
2024-05-08 19:15:33 +0800

    a larger buffer pool

--- BPTree.hpp
+++ BPTree.hpp
@@ -16,7 +16,7 @@ using std::ofstream;
 using std::vector;
 
 //each internal node with M keys and M+1 sons
-template<class T, int Max_Nodes = 3000, int M = 300, int Buffer_Size = 150>
+template<class T, int Max_Nodes = 3000, int M = 300, int Buffer_Size = 300>
 class BPTree{
 private:
     fstream file, file_value;
